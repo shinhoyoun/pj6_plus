a734770babbc447053b73722b1ea76a6cea499e6
sejinmac <lsjyahoo@gmail.com>
2026-01-02 20:49:33 +0900

    add: Store 파일 업로드 컨트롤러 생성 1. storeFileUploadController 생성 2.build.gradle 의존성 추가

---
+++ src/main/java/com/example/demo/domain/store/controller/StoreUploadContoller.java
@@ -0,0 +1,32 @@
+package com.example.demo.domain.store.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api/stores")
+@RequiredArgsConstructor
+@Slf4j
+public class StoreUploadContoller {
+
+
+
+    /**
+     * 파일 업로드
+     */
+    @PostMapping("/uploadForm")
+    public void uploadFormPostApi(@RequestPart("file") MultipartFile multipartFile) {
+
+        //1.csv 파일을 서버에 전송하기
+        //2.csv 파일을 자바 라이브러리를 이용해 읽을 수 있는 방법
+        //3.엔티티를 통해서 읽을 수 있게 만들기
+
+    }
+
+
+}
