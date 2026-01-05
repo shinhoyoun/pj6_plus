d041175d6a31445f0c653e05e985b89b3ef626bb
aram0117 <dkfka568355@gmail.com>
2026-01-05 10:54:00 +0900

    feat: 유저쿠폰 발급 기능 구현
    - 404, 409 언체크 예외

---
+++ src/main/java/com/example/demo/domain/usercoupon/service/UserCouponService.java
@@ -0,0 +1,49 @@
+package com.example.demo.domain.usercoupon.service;
+
+import com.example.demo.domain.coupon.entity.Coupon;
+import com.example.demo.domain.coupon.repository.CouponRepository;
+import com.example.demo.domain.user.entity.User;
+import com.example.demo.domain.user.repository.UserRepository;
+import com.example.demo.domain.usercoupon.dto.response.IssuedUserCouponResponseDto;
+import com.example.demo.domain.usercoupon.entitiy.UserCoupon;
+import com.example.demo.domain.usercoupon.repository.UserCouponRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserCouponService {
+    private final UserCouponRepository userCouponRepository;
+    private final CouponRepository couponRepository;
+    private final UserRepository userRepository;
+
+    @Transactional
+    public IssuedUserCouponResponseDto issuedCoupon(long couponId, long userId) {
+        Coupon foundCoupon = couponRepository.findById(couponId).orElseThrow(
+                () -> new RuntimeException("존재하지 않은 쿠폰입니다.")
+        );
+
+        User foundUser = userRepository.findById(userId).orElseThrow(
+                () -> new RuntimeException("존재하지 않은 유저입니다.")
+        );
+
+        boolean exists = userCouponRepository.existsByCouponIdAndUserId(couponId, userId);
+
+        if(exists) {
+            throw new RuntimeException("중복 발급이 불가합니다.");
+        }
+
+        UserCoupon userCoupon = new UserCoupon(foundCoupon, foundUser);
+
+        UserCoupon newUserCoupon = userCouponRepository.save(userCoupon);
+
+        foundCoupon.issuedCoupon();
+
+        foundCoupon.expiredCoupon();
+
+        long userCouponId = newUserCoupon.getId();
+
+        return new IssuedUserCouponResponseDto(userCouponId);
+    }
+}
