467a81c726317ababb68deba6ac708eb452e1fff
SHY <shy9729@gmail.com>
2026-01-05 15:54:29 +0900

    ✨ feat: user crud 구현
    bulid.gradle : crypto:7.0.2, bcrypt, sarter-validation 추가
    
    data 담을 CommonResponse 생성
    
    PasswordEncoder 구현
    
    User CRUD 구현 : (생성, 조회(단건, 전체), 수정, 탈퇴

---
+++ src/main/java/com/example/demo/domain/user/dto/request/UserCreateRequest.java
@@ -0,0 +1,28 @@
+package com.example.demo.domain.user.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+
+@Getter
+public class UserCreateRequest {
+
+    @NotBlank
+    @Size(min = 4, max = 20, message = "아이디를 4 ~ 20자로 입력하세요.")
+    @Pattern(regexp = "^[A-Za-z0-9]+$", message = "아이디는 영문/숫자만 허용합니다.")
+    private String username;
+
+    @NotBlank
+    @Email(message = "올바른 이메일 형식이 아닙니다")
+    private String email;
+
+    @NotBlank
+    @Size(min = 8, message = "비밀번호는 8자 이상으로 입력하세요")
+    private String password;
+
+    @NotBlank
+    @Size(min = 2, max = 30, message = "이름은 2 ~ 30자로 입력하세요")
+    private String name;
+}
