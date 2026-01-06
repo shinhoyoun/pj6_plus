5dab92acedcae0b2b1d8e07a5bbc2e3275654723
Leechungwoon <dlcjddns22@gmail.com>
2026-01-06 12:58:10 +0900

    feat: 빌드 시큐리티 의존성 제거 및 클래스 주석처리

--- src/main/java/com/example/demo/common/config/SecurityConfig.java
+++ src/main/java/com/example/demo/common/config/SecurityConfig.java
@@ -1,36 +1,50 @@
-package com.example.demo.common.config;
-
-import com.example.demo.common.auth.filter.JwtFilter;
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.servletapi.SecurityContextHolderAwareRequestFilter;
-
-
-@Configuration //Config 파일로 설정
-@RequiredArgsConstructor
-@EnableWebSecurity
-//@EnableMethodSecurity(securedEnabled = true)
-public class SecurityConfig {
-
-    private final JwtFilter jwtFilter;
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .csrf(AbstractHttpConfigurer::disable)
-                .httpBasic(AbstractHttpConfigurer::disable)
-                .formLogin(AbstractHttpConfigurer::disable)
-                .addFilterBefore(jwtFilter, SecurityContextHolderAwareRequestFilter.class)
-                .authorizeHttpRequests(auth -> auth
-                        // 회원가입, 로그인은 누구나 접근 가능
-                        .requestMatchers("/api/auth/**").permitAll()
-                        .anyRequest().authenticated() // 나머지는 로그인한 사용자만 접근 가능
-                )
-                .build();
-    }
-}
+//package com.example.demo.common.config;
+//
+//import com.example.demo.common.auth.filter.JwtFilter;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//import org.springframework.security.web.servletapi.SecurityContextHolderAwareRequestFilter;
+//
+//
+//@Configuration //Config 파일로 설정
+//@RequiredArgsConstructor
+//@EnableWebSecurity
+////@EnableMethodSecurity(securedEnabled = true)
+//public class SecurityConfig {
+//
+//    private final JwtFilter jwtFilter;
+//
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        return http
+//                .csrf(AbstractHttpConfigurer::disable)
+//                .httpBasic(AbstractHttpConfigurer::disable)
+//                .formLogin(AbstractHttpConfigurer::disable)
+//
+////                .addFilterBefore(jwtFilter, SecurityContextHolderAwareRequestFilter.class)
+//                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+//                .authorizeHttpRequests(auth -> auth
+//                        // 회원가입, 로그인은 누구나 접근 가능
+//                        .requestMatchers("/api/auth/**").permitAll()
+//                        .requestMatchers(HttpMethod.POST,"/api/users").permitAll() //회원가입 허용
+//                        .requestMatchers(HttpMethod.POST,"/api/reviews").authenticated() //로그인 . 댓글 생성 가능
+//                        .anyRequest().authenticated() // 나머지는 로그인한 사용자만 접근 가능
+//                )
+//                .build();
+//    }
+//}
