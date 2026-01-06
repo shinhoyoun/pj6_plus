0d61749e7c338228fdc149f4cfcae3b5ca159d09
Leechungwoon <dlcjddns22@gmail.com>
2026-01-06 12:58:48 +0900

    feat: 시큐리티 제외 주석 처리

--- src/main/java/com/example/demo/common/config/WebConfig.java
+++ src/main/java/com/example/demo/common/config/WebConfig.java
@@ -1,24 +1,24 @@
-package com.example.demo.common.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-@Configuration
-public class WebConfig {
-
-
-    /**
-     * PasswordEncoder 빈으로 등록
-     * @return
-     */
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-}
-// Create an encoder with strength 16
-//    BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(16);
-//    String result = encoder.encode("myPassword");
-//    assertTrue(encoder.matches("myPassword", result));
+//package com.example.demo.common.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//
+//@Configuration
+//public class WebConfig {
+//
+//
+//    /**
+//     * PasswordEncoder 빈으로 등록
+//     * @return
+//     */
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//}
+////// Create an encoder with strength 16
+//////    BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(16);
+//////    String result = encoder.encode("myPassword");
+//////    assertTrue(encoder.matches("myPassword", result));
