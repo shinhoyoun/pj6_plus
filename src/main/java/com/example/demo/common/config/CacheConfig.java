851507c99ccb1f85df005fcdd9fe5c149622eae0
SHY <shy9729@gmail.com>
2026-01-07 22:53:01 +0900

    ✨ feat: v1 - 키워드캐시저장 + 캐시에저장된 데이터 목록 조회
    CacheConfig - 사이즈, 주기 로 기본만 정해서 구현
    
    KeywordController - 키워드 저장, 목록조회 할 수 있게 keywordService.getKeywords() 반환
    
    KeywordService - null이나 공백있을시 return , 있으면 키워드 캐시에 저장
    or 저장된 키워드 List 받아옴

--- src/main/java/com/example/demo/common/config/CacheConfig.java
+++ src/main/java/com/example/demo/common/config/CacheConfig.java
@@ -1,5 +1,6 @@
 package com.example.demo.common.config;
 
+import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
@@ -13,22 +14,35 @@ import java.time.Duration;
 @EnableCaching // 캐시 기능 활성화 -> 없으면 캐시 활성화 안됨
 public class CacheConfig {
 
+    // v1
     @Bean
-    public CacheManager cacheManger() {
+    public Cache<String, Boolean> keywordCache() {
 
-        CaffeineCacheManager manager = new CaffeineCacheManager(
-                "searchCountCache",     // 키워드별 조회수
-                            "searchAbuseCache",     // 어뷰징 방지
-                            "searchRankCache"       // 인기검색어 랭킹
-        );
-
-        manager.setCaffeine(
-                Caffeine.newBuilder()
-                        .maximumSize(10_000)
-                        .expireAfterWrite(Duration.ofDays(1)) // 하루유지 (자정 리셋)
-        );
-
-        return manager;
+        return Caffeine.newBuilder()
+                .maximumSize(10000)
+                .expireAfterWrite(Duration.ofDays(1))
+                .build();
     }
+
+
+    // v2
+//    @Bean
+//    public CacheManager cacheManger() {
+//
+//        CaffeineCacheManager manager = new CaffeineCacheManager(
+//                "searchCountCache",     // 키워드별 조회수
+//                            "searchAbuseCache",     // 어뷰징 방지
+//                            "searchRankCache"       // 인기검색어 랭킹
+//        );
+//
+//        manager.setCaffeine(
+//                Caffeine.newBuilder()
+//                        .maximumSize(10_000)
+//                        .expireAfterWrite(Duration.ofDays(1)) // 하루유지 (자정 리셋)
+//        );
+//
+//        return manager;
+//    }
+
 
 }
