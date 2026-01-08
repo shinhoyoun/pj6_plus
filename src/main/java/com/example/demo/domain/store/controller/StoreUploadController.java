006ff9681c08035720191de989d33f5a407249e1
sejinmac <lsjyahoo@gmail.com>
2026-01-08 16:28:54 +0900

    feat: csv 파일 업로드 동기식 -> 비동기식 1.업로드 시간이 오래 걸리는 관계로 비동기식으로 전환 2.openApi 업로드시 상점명이 데이터베이스에 있으면 저장하지 않고 넘어감

--- src/main/java/com/example/demo/domain/store/controller/StoreUploadController.java
+++ src/main/java/com/example/demo/domain/store/controller/StoreUploadController.java
@@ -8,6 +8,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/api/stores")
 @RequiredArgsConstructor
@@ -22,11 +26,26 @@ public class StoreUploadController {
      */
     @PostMapping("/collerction")
     public ResponseEntity<String> uploadFormPostApi(@RequestPart("file") MultipartFile file) {
+        if (file.isEmpty()) {
+            return ResponseEntity.badRequest().body("파일이 비어있습니다.");
+        }
+
         try {
-            storeUploadService.uploadCsvFile(file);
-            return ResponseEntity.ok("업로드 완료");
+            // 임시 파일 생성
+            String tempFileName = UUID.randomUUID().toString() + "_" + file.getOriginalFilename();
+            File tempFile = File.createTempFile(tempFileName, null);
+            file.transferTo(tempFile);
+
+            // 비동기 처리 호출
+            storeUploadService.uploadCsvFile(tempFile);
+
+            return ResponseEntity.ok("파일 업로드가 시작되었습니다. 처리가 완료되면 로그를 확인하세요.");
+        } catch (IOException e) {
+            log.error("파일 저장 중 오류 발생", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("파일 저장 중 오류가 발생했습니다.");
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+            log.error("업로드 요청 처리 중 오류 발생", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("업로드 요청 처리 중 오류가 발생했습니다.");
         }
     }
 
--- src/main/java/com/example/demo/domain/store/service/StoreUploadService.java
+++ src/main/java/com/example/demo/domain/store/service/StoreUploadService.java
@@ -5,13 +5,15 @@ import com.example.demo.domain.store.repository.StoreRepository;
 import com.opencsv.CSVReader;
 import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.reactive.function.client.WebClient;
 import tools.jackson.databind.JsonNode;
 import tools.jackson.databind.ObjectMapper;
 
-import java.io.InputStreamReader;
+import java.io.File;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,88 +32,104 @@ public class StoreUploadService {
         this.webClient = webClientBuilder.baseUrl("http://openapi.seoul.go.kr:8088").build();
     }
 
-    public void uploadCsvFile(MultipartFile file) throws Exception {
-            try (CSVReader csvReader = new CSVReader(new InputStreamReader(file.getInputStream()))) {
+    @Async
+    public void uploadCsvFile(File file) throws Exception {
+            try (CSVReader csvReader = new CSVReader(new FileReader(file))) {
                 String[] nextRecord;
                 List<Store> stores = new ArrayList<>();
                 int batchSize = 1000;
+                int processedCount = 0;
 
                 csvReader.readNext();
 
                 while ((nextRecord = csvReader.readNext()) != null) {
-                   String companyName = nextRecord[0];
-                   String mallName = nextRecord[1];
-                   String domainName = nextRecord[2];
-                   String phoneNumber = nextRecord[3];
-                   String email = nextRecord[4];
-                   String salesRegNo = nextRecord[5];
-                   String businessType = nextRecord[6];
-                   String initialReportDate = nextRecord[7];
-                   String address = nextRecord[8];
-                   String status = nextRecord[9];
-                   String totalRating = nextRecord[10];
-                   String bizInfoRating = nextRecord[11];
-                   String withdrawalRating = nextRecord[12];
-                   String paymentRating = nextRecord[13];
-                   String termsRating = nextRecord[14];
-                   String privacyRating = nextRecord[15];
-                   String mainItem = nextRecord[16];
-                   String withdrawalPossibility = nextRecord[17];
-                   String initialScreenInfo = nextRecord[18];
-                   String paymentMethod = nextRecord[19];
-                   String termsCompliance = nextRecord[20];
-                   String privacyPolicy = nextRecord[21];
-                   String requestExtraInfo = nextRecord[22];
-                   String safetyService = nextRecord[23];
-                   String securityServer = nextRecord[24];
-                   String certificationMark = nextRecord[25];
-                   String deliveryDateDisplay = nextRecord[26];
-                   String deliveryFeeBurden = nextRecord[27];
-                   String complaintBoard = nextRecord[28];
-                   String memberWithdrawal = nextRecord[29];
-                   String siteOpenYear = nextRecord[30];
-                   String monitoringDate = nextRecord[31];
+                   try {
+                       String companyName = nextRecord[0];
+                       String mallName = nextRecord[1];
+                       String domainName = nextRecord[2];
+                       String phoneNumber = nextRecord[3];
+                       String email = nextRecord[4];
+                       String salesRegNo = nextRecord[5];
+                       String businessType = nextRecord[6];
+                       String initialReportDate = nextRecord[7];
+                       String address = nextRecord[8];
+                       String status = nextRecord[9];
+                       String totalRating = nextRecord[10];
+                       String bizInfoRating = nextRecord[11];
+                       String withdrawalRating = nextRecord[12];
+                       String paymentRating = nextRecord[13];
+                       String termsRating = nextRecord[14];
+                       String privacyRating = nextRecord[15];
+                       String mainItem = nextRecord[16];
+                       String withdrawalPossibility = nextRecord[17];
+                       String initialScreenInfo = nextRecord[18];
+                       String paymentMethod = nextRecord[19];
+                       String termsCompliance = nextRecord[20];
+                       String privacyPolicy = nextRecord[21];
+                       String requestExtraInfo = nextRecord[22];
+                       String safetyService = nextRecord[23];
+                       String securityServer = nextRecord[24];
+                       String certificationMark = nextRecord[25];
+                       String deliveryDateDisplay = nextRecord[26];
+                       String deliveryFeeBurden = nextRecord[27];
+                       String complaintBoard = nextRecord[28];
+                       String memberWithdrawal = nextRecord[29];
+                       String siteOpenYear = nextRecord[30];
+                       String monitoringDate = nextRecord[31];
 
 
-                    Store store = Store.builder()
-                            .companyName(companyName)
-                            .mallName(mallName)
-                            .domainName(domainName)
-                            .phoneNumber(phoneNumber)
-                            .email(email)
-                            .salesRegNo(salesRegNo)
-                            .businessType(businessType)
-                            .address(address)
-                            .status(status)
-                            .totalRating(Integer.valueOf(totalRating))
-                            .bizInfoRating(Integer.valueOf(bizInfoRating))
-                            .withdrawalRating(Integer.valueOf(withdrawalRating))
-                            .paymentRating(Integer.valueOf(paymentRating))
-                            .termsRating(Integer.valueOf(termsRating))
-                            .privacyRating(Integer.valueOf(privacyRating))
-                            .mainItem(mainItem)
-                            .withdrawalPossibility(withdrawalPossibility)
-                            .initialScreenInfo(initialScreenInfo)
-                            .paymentMethod(paymentMethod)
-                            .termsCompliance(termsCompliance)
-                            .privacyPolicy(privacyPolicy)
-                            .requestExtraInfo(requestExtraInfo)
-                            .safetyService(safetyService)
-                            .securityServer(securityServer)
-                            .certificationMark(certificationMark)
-                            .deliveryDateDisplay(deliveryDateDisplay)
-                            .deliveryFeeBurden(deliveryFeeBurden)
-                            .complaintBoard(complaintBoard)
-                            .memberWithdrawal(memberWithdrawal)
-                            .siteOpenYear(siteOpenYear)
-                            .build();
+                        Store store = Store.builder()
+                                .companyName(companyName)
+                                .mallName(mallName)
+                                .domainName(domainName)
+                                .phoneNumber(phoneNumber)
+                                .email(email)
+                                .salesRegNo(salesRegNo)
+                                .businessType(businessType)
+                                .address(address)
+                                .status(status)
+                                .totalRating(Integer.valueOf(totalRating))
+                                .bizInfoRating(Integer.valueOf(bizInfoRating))
+                                .withdrawalRating(Integer.valueOf(withdrawalRating))
+                                .paymentRating(Integer.valueOf(paymentRating))
+                                .termsRating(Integer.valueOf(termsRating))
+                                .privacyRating(Integer.valueOf(privacyRating))
+                                .mainItem(mainItem)
+                                .withdrawalPossibility(withdrawalPossibility)
+                                .initialScreenInfo(initialScreenInfo)
+                                .paymentMethod(paymentMethod)
+                                .termsCompliance(termsCompliance)
+                                .privacyPolicy(privacyPolicy)
+                                .requestExtraInfo(requestExtraInfo)
+                                .safetyService(safetyService)
+                                .securityServer(securityServer)
+                                .certificationMark(certificationMark)
+                                .deliveryDateDisplay(deliveryDateDisplay)
+                                .deliveryFeeBurden(deliveryFeeBurden)
+                                .complaintBoard(complaintBoard)
+                                .memberWithdrawal(memberWithdrawal)
+                                .siteOpenYear(siteOpenYear)
+                                .build();
 
-                    stores.add(store);
+                        stores.add(store);
+                        processedCount++;
 
-                    if (stores.size() >= batchSize) {
-                        storeRepository.saveAll(stores);
-                        stores.clear();
-                    }
+                        if (stores.size() >= batchSize) {
+                            storeRepository.saveAll(stores);
+                            stores.clear();
+                            log.info("Processed {} records", processedCount);
+                        }
+                   } catch (Exception e) {
+                       log.error("Error processing record: {}", e.getMessage());
+                   }
+                }
+                if (!stores.isEmpty()) {
+                    storeRepository.saveAll(stores);
+                    log.info("Processed {} records", processedCount);
+                }
+            } finally {
+                if (file.exists()) {
+                    file.delete();
                 }
             }
         }
@@ -144,47 +162,61 @@ public class StoreUploadService {
             List<Store> storeList = new ArrayList<>();
 
             for (JsonNode itemNode : rowNode) {
-                Store store = Store.builder()
-                        .companyName(itemNode.path("COMPANY").asText(null))
-                        .mallName(itemNode.path("SHOP_NAME").asText(null))
-                        .domainName(itemNode.path("DOMAIN_NAME").asText(null))
-                        .phoneNumber(itemNode.path("TEL").asText(null))
-                        .email(itemNode.path("EMAIL").asText(null))
-                        .salesRegNo(itemNode.path("UPJONG_NBR").asText(null))
-                        .businessType(itemNode.path("YPFORM").asText(null))
-                        .address(itemNode.path("COM_ADDR").asText(null))
-                        .status(itemNode.path("STAT_NM").asText(null))
-                        .totalRating(itemNode.path("TOT_RATINGPOINT").asInt(0))
-                        .bizInfoRating(itemNode.path("CHOGI_RATINGPOINT").asInt(0))
-                        .withdrawalRating(itemNode.path("CHUNG_RATINGPOINT").asInt(0))
-                        .paymentRating(itemNode.path("DEAL_RATINGPOINT").asInt(0))
-                        .termsRating(itemNode.path("PYOJUN_RATINGPOINT").asInt(0))
-                        .privacyRating(itemNode.path("SECURITY_RATINGPOINT").asInt(0))
-                        .mainItem(itemNode.path("SERVICE").asText(null))
-                        .withdrawalPossibility(itemNode.path("CHUNG").asText(null))
-                        .initialScreenInfo(itemNode.path("CHOGI").asText(null))
-                        .paymentMethod(itemNode.path("GYULJE").asText(null))
-                        .termsCompliance(itemNode.path("PYOJUN").asText(null))
-                        .privacyPolicy(itemNode.path("P_INFO_CARE").asText(null))
-                        .requestExtraInfo(itemNode.path("PER_INFO").asText(null))
-                        .safetyService(itemNode.path("DEAL_CARE").asText(null))
-                        .securityServer(itemNode.path("SSL_YN").asText(null))
-                        .certificationMark(itemNode.path("INJEUNG").asText(null))
-                        .deliveryDateDisplay(itemNode.path("BAESONG_YEJEONG").asText(null))
-                        .deliveryFeeBurden(itemNode.path("BAESONG").asText(null))
-                        .complaintBoard(itemNode.path("CLIENT_BBS").asText(null))
-                        .memberWithdrawal(itemNode.path("LEAVE").asText(null))
-                        .siteOpenYear(itemNode.path("KAESOL_YEAR").asText(null))
-                        .build();
+                try {
+                    String companyName = itemNode.path("COMPANY").asText(null);
 
-                storeList.add(store);
+                    if (storeRepository.existsByCompanyName(companyName)) {
+                        continue;
+                    }
+
+                    Store store = Store.builder()
+                            .companyName(companyName)
+                            .mallName(itemNode.path("SHOP_NAME").asText(null))
+                            .domainName(itemNode.path("DOMAIN_NAME").asText(null))
+                            .phoneNumber(itemNode.path("TEL").asText(null))
+                            .email(itemNode.path("EMAIL").asText(null))
+                            .salesRegNo(itemNode.path("UPJONG_NBR").asText(null))
+                            .businessType(itemNode.path("YPFORM").asText(null))
+                            .address(itemNode.path("COM_ADDR").asText(null))
+                            .status(itemNode.path("STAT_NM").asText(null))
+                            .totalRating(itemNode.path("TOT_RATINGPOINT").asInt(0))
+                            .bizInfoRating(itemNode.path("CHOGI_RATINGPOINT").asInt(0))
+                            .withdrawalRating(itemNode.path("CHUNG_RATINGPOINT").asInt(0))
+                            .paymentRating(itemNode.path("DEAL_RATINGPOINT").asInt(0))
+                            .termsRating(itemNode.path("PYOJUN_RATINGPOINT").asInt(0))
+                            .privacyRating(itemNode.path("SECURITY_RATINGPOINT").asInt(0))
+                            .mainItem(itemNode.path("SERVICE").asText(null))
+                            .withdrawalPossibility(itemNode.path("CHUNG").asText(null))
+                            .initialScreenInfo(itemNode.path("CHOGI").asText(null))
+                            .paymentMethod(itemNode.path("GYULJE").asText(null))
+                            .termsCompliance(itemNode.path("PYOJUN").asText(null))
+                            .privacyPolicy(itemNode.path("P_INFO_CARE").asText(null))
+                            .requestExtraInfo(itemNode.path("PER_INFO").asText(null))
+                            .safetyService(itemNode.path("DEAL_CARE").asText(null))
+                            .securityServer(itemNode.path("SSL_YN").asText(null))
+                            .certificationMark(itemNode.path("INJEUNG").asText(null))
+                            .deliveryDateDisplay(itemNode.path("BAESONG_YEJEONG").asText(null))
+                            .deliveryFeeBurden(itemNode.path("BAESONG").asText(null))
+                            .complaintBoard(itemNode.path("CLIENT_BBS").asText(null))
+                            .memberWithdrawal(itemNode.path("LEAVE").asText(null))
+                            .siteOpenYear(itemNode.path("KAESOL_YEAR").asText(null))
+                            .build();
+
+
+                    storeList.add(store);
+                } catch (Exception e) {
+                    log.error("Error processing OpenAPI item: {}", e.getMessage());
+                }
             }
 
-            storeRepository.saveAll(storeList);
+            if (!storeList.isEmpty()) {
+                storeRepository.saveAll(storeList);
+            }
 
         } catch (Exception e) {
             throw new RuntimeException("JSON 파싱 중 오류가 발생했습니다. 데이터: " + jsonString, e);
         }
 
     }
+
 }
